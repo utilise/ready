84ac838e5c340facbbdcff5a0de0fab2c6852109
pemrouz <p3pedram@hotmail.com>
2015-12-13 22:10:50 +0000

    init commit

---
+++ test.js
@@ -0,0 +1,32 @@
+var expect = require('chai').expect
+  , ready  = require('./')
+  
+describe('once', function() {
+  
+  it('should invoke if ready', function(done) {
+    global.document = { body: true }
+
+    ready(function(){
+      expect(document.body).to.be.ok
+      done()
+    })
+  })
+
+  it('should invoke when ready', function(done) {
+    global.document = {} 
+
+    document.addEventListener = function(type, fn) {
+      expect(type).to.eql('DOMContentLoaded')
+      expect(fn).to.eql(callback)
+      fn(document.body = true)
+    }
+
+    ready(callback)
+
+    function callback(){
+      expect(document.body).to.be.ok
+      done()
+    }
+  })
+
+})
